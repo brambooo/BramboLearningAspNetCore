bd19aa3b4b3edbf01cdfa227bb4ec2668e443657
brambooo <bram_reinold@hotmail.com>
2017-04-22 20:06:52 +0200

    init commit, added also custom service that can read from json file

---
+++ BramboLearningCore/Startup.cs
@@ -0,0 +1,47 @@
+﻿using BramboLearningCore.services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace BramboLearningCore
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(env.ContentRootPath).AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Implemented a custom service that can be used
+            services.AddSingleton(provider => Configuration);
+            services.AddSingleton<IBramboService, BramboService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IBramboService bramboService)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                string title = bramboService.GetTitle();
+                await context.Response.WriteAsync(title);
+            });
+        }
+    }
+}
---
+++ BramboLearningCore/services/BramboService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BramboLearningCore.services
+{
+    public class BramboService : IBramboService
+    {
+        private string _intro;
+
+        public BramboService(IConfiguration configuration)
+        {
+            _intro = configuration["intro"];
+        }
+
+        public string GetTitle()
+        {
+            return _intro;
+        }
+
+    }
+}
